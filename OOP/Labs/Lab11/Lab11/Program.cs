6374cf32cbd4068b1bffc46e3b1d51bcc25fec3d
KuchN <mrkola45@gmail.com>
2024-11-15 16:43:26 +0300

    :added OOP lab11

--- OOP/Labs/Lab11/Lab11/Program.cs
+++ OOP/Labs/Lab11/Lab11/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Lab11;
+using Lab4;
+Lion lion = new Lion("biba", 0, 0, 2022, 200);
+string  assbl =Reflector.AssemblyName(lion);
+Console.WriteLine(assbl);
+bool pub_constr = Reflector.IsPubConstr(lion);
+Console.WriteLine(pub_constr);
+var list = Reflector.GetPublicMeth(lion);
+Console.WriteLine();
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+};
+Console.WriteLine();
+list = Reflector.publicPropAndFields(lion);
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+list = Reflector.publicPropAndFields(lion);
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+list = Reflector.GetInterfaces(lion);
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+int a=5;
+Reflector.PrintMeth(lion,a);
+Reflector.Invoke(lion, "MoveTo",new object []{ 0,0});
+Console.WriteLine(lion.X+" "+lion.Y);
+
+var bro =Reflector.Create(lion, new object[] { "boba", 5, 2, 2024, 300 });
+Console.WriteLine(bro);
---
+++ OOP/Labs/Lab11/Lab11/Reflector.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Lab11
+{
+    public static class Reflector//<T> where T : class
+    {
+        public static  string AssemblyName( object t)
+        {
+            Type type = t.GetType();
+            return type.Assembly.FullName;
+        }
+        public static bool IsPubConstr( object t ) 
+        {
+            Type t2 = t.GetType();
+            var constructors = t2.GetConstructors();
+            if ( constructors.Length > 0 ) {return true;}
+            return false;
+            
+        }
+        public static List<string> GetPublicMeth(object t) 
+        {
+            Type t2 = t.GetType();
+            List<string> list = new List<string>();
+            var methods =  t2.GetMethods();
+            foreach (var m in methods) 
+            {
+                list.Add( m.Name );
+            }
+            return list;
+        }
+        public static List<string> publicPropAndFields(object t)
+        {
+            Type t2 = t.GetType();
+            List<string> list = new List<string>();
+            var props = t2.GetProperties();
+            var fields = t2.GetFields();
+            foreach (var m in props)
+            {
+                list.Add(m.Name);
+            }
+            foreach (var m in fields)
+            {
+                list.Add(m.Name);
+            }
+            return list;
+        }
+        public static List<string> GetInterfaces(object t)
+        {
+            Type t2 = t.GetType();
+            List<string> list = new List<string>();
+            var interfaces = t2.GetInterfaces();
+            foreach (var m in interfaces)
+            {
+                list.Add(m.Name);
+            }
+            return list;
+        }
+        public static void PrintMeth(object t,object p)
+        {
+            Type t2 = t.GetType();
+            Type p1 = p.GetType();
+            List<string> list = new List<string>();
+            var methods = t2.GetMethods();
+            foreach (var m in methods)
+            {
+                bool check = false;
+                var parms = m.GetParameters();
+                foreach (var p2 in parms)
+                {
+                    if (p2.ParameterType== p1) { check= true; break; }
+                }
+
+                if (check){ Console.WriteLine(m.Name); }
+            }
+        }
+        public static void Invoke(object t,string name, object[] parm )
+        {
+            Type t2 = t.GetType();
+            Random rand = new Random();
+            var method = t2.GetMethod(name);
+            if (method == null) { return; }
+            for (int i = 0; i < parm.Length; i++)
+            {
+                parm[i] = rand.Next(20);
+            }
+            method.Invoke(t,parm);
+        }
+
+        public static object Create(object t, object[] parm)
+        {
+            Type type = t.GetType();
+            var obj = Activator.CreateInstance(type,parm);
+            return obj;
+        }
+    }
+}
