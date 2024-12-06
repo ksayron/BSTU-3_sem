a51497275c5bda0ae31ee95049086008670a6a34
KuchN <mrkola45@gmail.com>
2024-12-06 23:51:06 +0300

    finished SYP 11

--- SYP/Labs/lab11/sсript.js
+++ SYP/Labs/lab11/sсript.js
@@ -26,16 +26,38 @@ class ToDoList{
     }
 
     addTask(task){
-        this.list+=task;
+        this.list.push(task);
     }
 
     removeTask(task){
-        this.list-=task;
+        this.list.pop(task);
+    }
+
+    showTasks(){
+        console.group("Tasks");
+        this.list.forEach((element) => console.log(element))
+        console.groupEnd();
+    }
+    showTasksFilter(status){
+        if(status){console.group("Done tasks");}
+        else{console.group("Undone tasks");}
+        this.list.forEach((element) => {if(element.status==status){console.log(element)}})
+        console.groupEnd();
     }
 }
 
 let todolist = new ToDoList("list",1);
 
 let task1 = new Task("SYP Lab 11",1);
+let task2 = new Task("SYP Lab 12",2);
+let task3 = new Task("OOP Lab 16",3);
+
 todolist.addTask(task1);
+todolist.addTask(task2);
+todolist.addTask(task3);
+task3.setStatusTrue();
+//todolist.removeTask(task1);
 console.log(todolist);
+todolist.showTasks();
+todolist.showTasksFilter(false)
+todolist.showTasksFilter(true)
